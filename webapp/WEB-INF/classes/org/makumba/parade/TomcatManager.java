d5b3ceb683a18221ec2904c4a09e474177aa5ce4
Cristian Bogdan <cristi@csc.kth.se>
2005-11-04 18:13:24 +0000

    removed useless imports

--- webapp/WEB-INF/classes/org/makumba/parade/TomcatManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/TomcatManager.java
@@ -2,8 +2,6 @@ package org.makumba.parade;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
